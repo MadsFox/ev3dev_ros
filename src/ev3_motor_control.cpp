46c2f52729b8238ea452fcb7515ec1d3421ba4ad
MadsFox <mads.saustrup.kristensen@gmail.com>
2017-10-27 09:55:59 +0000

    optimizing motor control

--- src/ev3_motor_control.cpp
+++ src/ev3_motor_control.cpp
@@ -3,6 +3,7 @@
 #include "std_msgs/String.h"
 #include "ev3dev_ros/MotorCommands.h"
 
+#include <errno.h>
 #include <sstream>
 
 using namespace std;
@@ -17,9 +18,9 @@ class Motors{
     }
 
     void motorCommandCallback(MotorCommands mc){
-      update_command(mc.speed, mc.direction);
+      update_command(mc);
   
-      ROS_INFO("speed: %i - direction: %i", speed, direction);
+      //ROS_INFO("speed: %i - direction: %i", mc.speed, mc.direction);
       /*Place the ROS logic to run the motors here*/
     }
   private:
@@ -29,6 +30,7 @@ class Motors{
 
     void connect_motors()
     {
+      ROS_INFO("connecting motors");
       motor arrMotors[4] {
         { OUTPUT_A },
         { OUTPUT_B },
@@ -37,37 +39,44 @@ class Motors{
       };
       for (unsigned i=0; i<4; ++i)
       {
+        ROS_INFO("finding motors");
         motor &m = arrMotors[i];
         if (m.connected() && m.driver_name() == "lego-ev3-l-motor" && 
             m.address() == right_motor_port)
         {
+          ROS_INFO("pushing right motor tp motors_ motors");
           motors_.push_back(m);
         }else if (m.connected() && m.driver_name() == "lego-ev3-l-motor" && 
             m.address() == left_motor_port)
         {
+          ROS_INFO("pushing left motor tp motors_ motors");
           motors_.push_back(m);
         }else{
-          ROS_INFO("No motor connected to: %s", arrMotors[i].c_str());
+          ROS_INFO("No motor connected to: %s", arrMotors[i]);
         }
       }      
       //ROS_INFO("no motors connected of type: %s", motor_type.c_str());
     }
     void set_command(string command){
+      ROS_INFO("setting command: %s", command);
       for (unsigned j=0; j<2; j++){
         motor &mo = motors_[j];
         mo.set_command(command);
       }
     }
 
-    void update_command(int &speed, int &direction){
-      for (unsigned j=0; j<2; ++j){
-        motor &mo = motors_[j];
-        if(mo.address() == right_motor_port){
-          mo.set_duty_cycle_sp(speed-direction);
-        }else if(mo.address() == left_motor_port){
-          mo.set_duty_cycle_sp(speed+direction);
+    void update_command(MotorCommands mc){
+      try{
+        for (unsigned j=0; j<2; ++j){
+          motor &mo = motors_[j];
+          if(mo.address() == right_motor_port){
+            ROS_INFO("right motor speed: %i - direction: %i", mc.speed, mc.direction);
+            mo.set_duty_cycle_sp(max(-100, min(mc.speed+mc.direction, 100)));
+          }else if(mo.address() == left_motor_port){
+            mo.set_duty_cycle_sp(max(-100, min(mc.speed-mc.direction, 100)));
+          }
         }
-      }
+      }catch(...) { cout << "[" << strerror(errno) << "]" << endl; }
     }
 };
 
@@ -81,7 +90,7 @@ int main(int argc, char **argv)
 
   Motors ev3_motors(OUTPUT_A, OUTPUT_B);
 
-  ros::Subscriber sub = n.subscribe("motor_command", 1000,
+  ros::Subscriber sub = n.subscribe("motor_command", 50,
                                     &Motors::motorCommandCallback, &ev3_motors);
 
   ros::spin();
